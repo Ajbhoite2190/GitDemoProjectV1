b05779f23deed63509d361ceb785851cbb816b0f
44777 <Ajbhoite2190>
2022-08-29 14:04:40 +0530

    Add additional checks and remove the module method

--- src/defaultpackage/Test.java
+++ src/defaultpackage/Test.java
@@ -4,7 +4,7 @@ public class Test
 	public static void main(String[]args) {
 		
 		String hungry="Yes";
-		if(hungry=="yes")
+		if(hungry=="yes" || hungry=="yes" || hungry=="y")
 			System.out.println("Eat someting...");
 		else {
 			System.out.println("Do work..");
