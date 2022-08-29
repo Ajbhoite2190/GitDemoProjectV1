53f2f3391399faac76ed3ad41ad56f8b149d061a
44777 <Ajbhoite2190>
2022-08-29 13:32:12 +0530

    Commiting Test2 to BranchX

---
+++ src/defaultpackage/Test2.java
@@ -0,0 +1,14 @@
+package defaultpackage;
+public class Test2
+{
+	public static void main(String[]args) {
+		
+		String hungry="Yes";
+		if(hungry=="yes")
+			System.out.println("Eat someting...");
+		else {
+			System.out.println("Do work..");
+		}
+}
+	
+}
